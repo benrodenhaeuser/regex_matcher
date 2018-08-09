a02c579ea1dfcfc152a4aa58bc976ccfc1a04869
Ben Rodenhäuser <ben@karo.berlin>
2018-08-09 02:20:45 +0200

    move processing to new Matcher class

--- lib/engine.rb
+++ lib/engine.rb
@@ -1,8 +1,7 @@
 require_relative './string.rb'
 require_relative './parser.rb'
 require_relative './automaton.rb'
-require_relative './line.rb'
-require_relative './match.rb'
+require_relative './matcher.rb'
 
 module Rex
   class Engine
@@ -18,23 +17,15 @@ module Rex
       @path         = path
       @substitution = substitution
       @opts         = DEFAULT_OPTIONS.merge(user_options)
-      @automaton    = Parser.new(@pattern).parse.to_automaton.to_dfa
     end
 
     def run
+      automaton = Parser.new(@pattern).parse.to_automaton.to_dfa
       file = File.open(@path)
-      @line_number = 1
+      matcher = Matcher.new(automaton, line_count, @opts)
 
       until file.eof?
-        @line = Line.new(file.gets.chomp)
-        @matches = []
-
-        find_matches
-        process_matches
-        prepend_line_number
-        output
-
-        @line_number += 1
+        matcher.match(file.gets.chomp)
       end
 
       file.close
@@ -42,72 +33,8 @@ module Rex
 
     private
 
-    def find_matches
-      while @line.cursor
-        @match = Match.new(@line.position)
-        @automaton.reset
-
-        find_match
-
-        if @match.found?
-          @matches << @match
-          break unless @opts[:global_matching]
-        end
-
-        @line.position = [@match.from + 1, @match.to].compact.max
-      end
-    end
-
-    def find_match
-      while @automaton.step?(@line.cursor)
-        @automaton.step!(@line.cursor)
-        @line.consume
-        @match.to = @line.position if @automaton.terminal?
-      end
-    end
-
-    def process_matches
-      @matches.reverse_each do |match|
-        process_match(match)
-      end
-    end
-
-    def process_match(match)
-      pre        = @line[0...match.from]
-      the_match  = @line[match.from...match.to]
-      post       = @line[match.to...@line.length]
-      @line.text = pre + replace(the_match) + post
-    end
-
-    def replace(the_match)
-      if tty? && @substitution
-        @substitution.red.underline
-      elsif tty?
-        the_match.red.underline
-      elsif @substitution
-        @substitution
-      else
-        the_match
-      end
-    end
-
-    def tty?
-      $stdout.isatty
-    end
-
-    def prepend_line_number
-      return unless @opts[:line_numbers]
-      pad = format("%0#{line_count.length}d:  ", @line_number.to_s)
-      @line.text = pad + @line.text
-    end
-
     def line_count
       @line_count ||= `wc -l #{@path}`.split.first
-    end
-
-    def output
-      return unless !@matches.empty? || @opts[:non_matching_lines]
-      puts @line
     end
   end
 end
---
+++ lib/matcher.rb
@@ -0,0 +1,91 @@
+require_relative './line.rb'
+require_relative './match.rb'
+
+module Rex
+  class Matcher
+    def initialize(automaton, line_count, opts)
+      @automaton = automaton
+      @opts = opts
+      @line_count = line_count
+      @line_number = 0
+    end
+
+    def match(text)
+      @line = Line.new(text)
+      @line_number += 1
+
+      find_matches
+      process_matches
+      prepend_line_number
+      output
+    end
+
+    private
+
+    def find_matches
+      @matches = []
+
+      while @line.cursor
+        @match = Match.new(@line.position)
+        @automaton.reset
+
+        find_match
+
+        if @match.found?
+          @matches << @match
+          break unless @opts[:global_matching]
+        end
+
+        @line.position = [@match.from + 1, @match.to].compact.max
+      end
+    end
+
+    def find_match
+      while @automaton.step?(@line.cursor)
+        @automaton.step!(@line.cursor)
+        @line.consume
+        @match.to = @line.position if @automaton.terminal?
+      end
+    end
+
+    def process_matches
+      @matches.reverse_each do |match|
+        process_match(match)
+      end
+    end
+
+    def process_match(match)
+      pre        = @line[0...match.from]
+      the_match  = @line[match.from...match.to]
+      post       = @line[match.to...@line.length]
+      @line.text = pre + replace(the_match) + post
+    end
+
+    def replace(the_match)
+      if tty? && @substitution
+        @substitution.red.underline
+      elsif tty?
+        the_match.red.underline
+      elsif @substitution
+        @substitution
+      else
+        the_match
+      end
+    end
+
+    def tty?
+      $stdout.isatty
+    end
+
+    def prepend_line_number
+      return unless @opts[:line_numbers]
+      pad = format("%0#{@line_count.length}d:  ", @line_number.to_s)
+      @line.text = pad + @line.text
+    end
+
+    def output
+      return unless !@matches.empty? || @opts[:non_matching_lines]
+      puts @line
+    end
+  end
+end
