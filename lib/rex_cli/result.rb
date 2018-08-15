a3a94c666b8b3658ce6229edb73693695f8f7f85
Ben Rodenhäuser <ben@karo.berlin>
2018-08-15 22:27:08 +0200

    refactor Result reporting methods

--- lib/rex_cli/result.rb
+++ lib/rex_cli/result.rb
@@ -25,58 +25,43 @@ module Rex
     private
 
     def only_matches_report
-      my_report = @matches.map do |match|
-        # TODO: refactor concatenation logic
-        if prefix.empty?
-          highlight(@text[match.from...match.to], :red_underline)
-        else
-          [
-            highlight(prefix(match), :dim),
-            highlight(@text[match.from...match.to], :red_underline)
-          ].join(" ")
-        end
+      the_matches = @matches.map do |match|
+        highlight(@text[match.from...match.to], :red_underline)
       end
-      my_report.map!(&:lstrip) unless @opts[:whitespace]
-      my_report.join("\n")
+
+      the_prefixes = @matches.map do |match|
+        highlight(prefix(match), :dim)
+      end
+
+      the_prefixes.zip(the_matches).map(&:join).join("\n")
     end
 
     def text_with_matches_report
-      my_report = @matches.reverse.inject(@text) do |text, match|
+      the_line = @matches.reverse.inject(@text) do |text, match|
         pre_match  = text[0...match.from]
         the_match  = text[match.from...match.to]
         post_match = text[match.to...text.length]
 
         pre_match + highlight(the_match, :red_underline) + post_match
       end
-      my_report.lstrip! unless @opts[:whitespace]
 
-      # TODO: refactor concatenation logic
-      if prefix.empty?
-        my_report
-      else
-        [highlight(prefix, :dim), my_report].join(" ")
-      end
+      the_line.lstrip! unless @opts[:whitespace]
+
+      highlight(prefix, :dim) + the_line
     end
 
     def prefix(match = nil)
-      return filename unless @opts[:line_numbers]
+      file = @opts[:filename]                             ? @filename      : nil
+      row  = @opts[:line_numbers]                         ? @lineno        : nil
+      col  = @opts[:only_matches] && @opts[:line_numbers] ? match.from + 1 : nil
 
-      if match
-        "#{filename}#{lineno}:#{match.from + 1}:"
-      else
-        "#{filename}#{lineno}:"
-      end
-    end
-
-    def filename
-      @opts[:file_names] ? "#{File.basename(@filename)}:" : ""
-    end
-
-    def lineno
-      @lineno
+      the_prefix = [file, row, col].compact.join(':')
+      the_prefix.empty? ? the_prefix : the_prefix + ': '
     end
 
     def highlight(text, style)
+      return text if text.empty?
+
       case @opts[:highlight]
       when true then text.highlight(style)
       when false then text
